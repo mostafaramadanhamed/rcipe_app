b8712258358bb19564fdd447c9902b53ebb400cd
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-03-18 11:35:59 +0200

    add top bar

--- lib/core/constant/app_strings.dart
+++ lib/core/constant/app_strings.dart
@@ -1,4 +1,7 @@
 class AppString{
-  static const kSplashText='Savory &\n Sweet';
+  static const kSplashText='Savory &\nSweet';
   static const kAppName='Recipe App';
+  static const kSearch='Search';
+  static const kTitleBar='What is in your kitchen?';
+  static const kSubTitleBar='Enter some ingredients';
 }
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rcipe_app/core/constant/app_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   static const routeName='/home-screen';
@@ -6,6 +7,39 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final size=MediaQuery.of(context).size;
+  return Scaffold(
+    backgroundColor: const Color(0xffFFFFFF),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: size.height/10,
+            ),
+            const Center(
+              child:  Text(AppString.kSearch,style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+              ),),
+            ),
+            SizedBox(
+              height: size.height/20,
+            ),
+            const Text(AppString.kTitleBar,style:  TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w600,
+            ),),  const  SizedBox(
+              height: 10,
+            ),
+            const Text(AppString.kSubTitleBar,style:  TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+            ),)
+          ],
+        ),
+      ),
+    );
   }
 }
