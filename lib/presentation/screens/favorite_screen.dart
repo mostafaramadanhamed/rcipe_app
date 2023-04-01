42aa1e876c75b2bad3b06a683f410bf4cfa717ca
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2023-04-01 10:54:17 +0200

    add favorite screen

--- lib/presentation/screens/favorite_screen.dart
+++ lib/presentation/screens/favorite_screen.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
+import '../../core/constant/app_strings.dart';
+import '../widgets/home/build_title.dart';
+
 class FavoriteScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery
+        .of(context)
+        .size;
     return Column(
       children: [
-
+        SizedBox(
+          height: size.height / 12,
+        ),
+        Center(
+          child: buildTextTitle(AppString.kFavoriteTitle),
+        ),
       ],
-    )
+    );
   }
 }
